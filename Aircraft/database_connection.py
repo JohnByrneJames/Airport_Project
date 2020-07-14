59b98867e7983fa6ead9e29b89d11b55a8667a32
John Byrne <neardem1@gmail.com>
2020-07-14 17:16:08 +0100

    Pushing changes

--- Aircraft/database_connection.py
+++ Aircraft/database_connection.py
@@ -1,21 +1,26 @@
-import os
 import pyodbc
 
 class DatabaseConnector:
+    # Essential attributes needed by database connector to make a connection {Server, Database, Username, Password}
+    # Connection_string formulated using credentials and then a cursor that is used to make the retrieval of data from
+    # the database.
     __server = None
     __database = None
     __username = None
     __password = None
     __connection_string = None
     __cursor = None
+    retry_timer = 0
 
     def __init__(self, server, database, username, password):
         self.__server = server
         self.__database = database
         self.__username = username
         self.__password = password
+        self.retry_timer = 0  # This is used to count the attempts to connect to the database
 
     def establish_connection(self):  # establishing connection
+        # Create, store and connect to the database using this connection string
         connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=" \
                            + self.__server + ";DATABASE=" + self.__database + \
                            ";UID=" + self.__username + ";PWD=" + self.__password
@@ -24,18 +29,21 @@ class DatabaseConnector:
         try:
             with pyodbc.connect(self.__connection_string, timeout=10) as connection:  # Connection to database
                 # Success and connection has been made
-                print("Connection successfully established..!")
-        except (ConnectionError, pyodbc.OperationalError, pyodbc.DatabaseError):
-            print("connection has timed out, or the database was not available")
-            return
+                print("Connection successfully established..!")  # Connection has been established, continue.
+        except (ConnectionError, pyodbc.OperationalError, pyodbc.DatabaseError):  # Catch common errors faced in pyodbc
+            return "connection has timed out, or the database was not available - " \
+                   "\nNo point in retrying... Please review connection credentials"
         except pyodbc.InterfaceError:
             # This is an error that is raised when the database interface is not available.
             print("Invalid connection to DB interface")
-            return
-        except Exception:
+            print("\nTrying again...")  # Try reconnecting to the database again.
+            if self.retry_timer == 3:  # After 2 attempts exit
+                return "Error encountered"
+            self.retry_timer += 1  # Increase the retry timer by 1
+            self.establish_connection()  # retry establishing connection by calling itself
+        except Exception:  # base Exception makes this fall proof
             # This prevents exceptions falling through the exceptions handler (as this catches all possible exceptions)
-            print("There was an error that occurred during the connection attempt...")
-            return
+            return "There was an error that occurred during the connection attempt..."
         else:
             cursor = connection.cursor()  # The cursor is a control structure
 
--- Aircraft/main.py
+++ Aircraft/main.py
@@ -1,12 +1,6 @@
 import os
 from database_connection import DatabaseConnector
 
-# Connection credentials
-server = os.environ.get("db_server")  # Server name, this here gets the OS private environment variables
-database = "JMS_AirportDatabase"  # Database name, this here gets the OS private environment variables
-# Database Login credentials
-username = os.environ.get("db_username")  # username, this here gets the OS private environment variables
-password = os.environ.get("db_password")  # password, this here gets the OS private environment variables
-
-database_link = DatabaseConnector(server, database, username, password)  # create instance with credentials
-database_link.establish_connection()
+database_link = DatabaseConnector(os.environ.get("db_server"), "JMS_AirportDatabase", os.environ.get("db_username"),
+                                  os.environ.get("db_password"))  # create instance with credentials
+database_link.establish_connection()  # Use instance to establish connection
