0d977304d6749f78dd8fd62c118616f8c5b2e825
S Sohail <ssohail@spartaglobal.com>
2020-07-16 15:15:49 +0100

    UPDATE CODE

--- Sohaib's Terminal/main.py
+++ Sohaib's Terminal/main.py
@@ -1,8 +1,11 @@
 from passengerinput import Passengers
-from new_connection import databaseconnection
+from Database_Connection.database_connection import DatabaseConnector
 
-cursor = databaseconnection()
+datalink = DatabaseConnector("databases.spartaglobal.academy", "JMS_AirportDatabase", "SA", "Passw0rd2018")
 
+cursor = datalink.establish_connection()
 object = Passengers(cursor)
 # object.customer_input()
-object.choice_input()
+# object.choice_input()
+
+object.customer_booking_itself()
--- Sohaib's Terminal/passengerinput.py
+++ Sohaib's Terminal/passengerinput.py
@@ -1,4 +1,4 @@
-from new_connection import databaseconnection
+from Database_Connection.database_connection import DatabaseConnector
 
 from datetime import datetime
 # As an airport assistant, I want to create passengers with name AND passport number, so that I can add them to the flight. - Sohaib Sohail
@@ -12,6 +12,7 @@ class Passengers():
     dateofbirth_input = None
     firstname_input = None
     surname_input = None
+    destination_input = None
 
     def __init__(self, cursor):
         self.cursor = cursor
@@ -25,13 +26,15 @@ class Passengers():
         self.customer_surname = str(input("Please enter your surname: \n").title())
         self.customer_dateofbirth = input("Please enter your date of birth. \n *Please enter the date in this format: YYYY-MM-DD* \n")
 
-        while True:
-            try:
-                datetime.strptime(self.customer_dateofbirth, '%Y-%m-%d')
-                print("This is the correct date string format.")
-            except ValueError:
-                print("This is the incorrect date string format. It should be YYYY-MM-DD")
-            break
+        # while True:
+        #     try:
+        #          datetime.strptime(self.customer_dateofbirth, '%Y-%m-%d')
+        #     except ValueError:
+        #         print("This is the incorrect date string format. It should be YYYY-MM-DD")
+        #         continue
+        #     else:
+        #         print("This is the correct date string format.")
+        #     break
 
         sql_customer_query = ("INSERT INTO Customers(FirstName, LastName, DateOfBirth) VALUES(?, ?, ?)")
 
@@ -60,20 +63,40 @@ class Passengers():
 
         print("As you have already entered a few details, you only need to add your passport number, and the choice of destination to proceed")
 
+        # sql_query = ("SELECT * FROM Flights")
+        # flights = self.cursor.execute(sql_query)
+        # countries = []
+        #
+        # for flight in flights:
+        #     destination = (flight.Destination)
+        #     print("Your destination is" + destination)
+        #     print(flight.DepartureDate)
+        #     print(flight.)
+        #     # print(flights)
+
+
+        self.destination_input = (input("What is your preferred destination? Please enter the same value in the table. \n"))
+
         self.firstname_input = self.customer_firstname
         self.surname_input = self.customer_surname
-        self.passportnum_input = (input("What is the person's passport number?"))
+
+        while True:
+
+            self.passportnum_input = (input("What is your passport number?\n"))
+
+            try:
+                if len(self.passportnum_input) != 9:
+                    raise ValueError
+            except ValueError:
+                print("Please enter the correct number")
+                continue
+            else:
+                print("Successfully accepted your passport number!")
+                break
+
         self.dateofbirth_input = self.customer_dateofbirth
 
-        try:
-            if len(self.passportnum_input) == 9 and type(self.passportnum_input) == int:
-                print("Successfully accepted your passport number")
-        except ValueError:
-            print("Please enter a number")
-        else:
-            print("Sorry, you have not entered the number correctly")
-
-        sql_self_customer_query = ("INSERT INTO BookingDetails(FirstName, LastName, PassportNum, DateOfBirth) VALUES(?, ?, ?, ?)")
+        sql_self_customer_query = ("INSERT INTO BookingDetails(FlightID, FirstName, LastName, PassportNum, DateOfBirth) VALUES(?, ?, ?, ?, ?)")
 
         self.cursor.execute(sql_self_customer_query, self.customer_firstname, self.customer_surname, self.passportnum_input, self.customer_dateofbirth)
         self.cursor.commit()
@@ -120,51 +143,6 @@ class Passengers():
         print("You have successfully added a passenger to the flight list")
 
 
-    # def dateofbirth_test_passengers(self):
-    #     while True:
-    #         try:
-    #             datetime.strptime(self.dateofbirth_input, '%Y-%m-%d')
-    #             print("This is the correct date string format.")
-    #         except ValueError:
-    #             print("This is the incorrect date string format. It should be YYYY-MM-DD")
-    #         break
-    #
-    #
-    # def dateofbirth_test_customers(self):
-    #     while True:
-    #         try:
-    #             datetime.strptime(self.customer_dateofbirth, '%Y-%m-%d')
-    #             print("This is the correct date string format.")
-    #         except ValueError:
-    #             print("This is the incorrect date string format. It should be YYYY-MM-DD")
-    #         break
-    #
-    #
-    # def changing_booking_details(self):
-    #
-    #     user_choice = int(input("Welcome to the amending booking details menu. Please select one of the following options:"
-    #               "\n 1. Do you want to change the destination of your flight? \n 2. Do you want to book a flight for someone else? \n 3. Change of mind? Do you want to edit a pre-booked flight? \n"))
-    #
-    #
-    # def passportnum_test(self):
-    #     try:
-    #         if len(self.passportnumber_input) == 9 and type(self.passportnumber_input) == int:
-    #             print("Successfully accepted your passport number")
-    #     except ValueError:
-    #         print("Please enter a number")
-    #     else:
-    #         print("Sorry, you have not entered the number correctly")
-    #
-
-#
-#     def passenger_input_in_flights(self):
-#
-#         sql_query3 = ("INSERT INTO BookingDetails(FirstName, LastName, PassportNum, DateOfBirth) VALUES(" + str(self.firstname_input) + str(self.surname_input) + (self.passportnumber_input) + self.dateofbirth_input + ")") #This is to insert data into the flights information
-#
-#         self.cursor.execute(sql_query3)
-#         self.cursor.commit()
-#
-#
 # #Buying a ticket for yourself. Entering details as a customer before buying a ticket
 # #Buying it for someone else - customer details and passenger details
 #
