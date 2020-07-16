d20a165b54b898f8b3729715aaa556f2aa4e9bbc
S Sohail <ssohail@spartaglobal.com>
2020-07-16 09:10:58 +0100

    Interfacecode

--- Sohaib's Terminal/passengerinput.py
+++ Sohaib's Terminal/passengerinput.py
@@ -6,11 +6,79 @@ from datetime import datetime
 class Passengers():
 
     def __init__(self, cursor):
-        self.firstname_input = (input(str(print("What is your first name?"))).title())
-        self.surname_input = (input(str(print("What is your surname?"))).title())
-        self.dateofbirth_input = (input(print("What is your date of birth? *Please enter the date in this format: YYYY-MM-DD*")))
-        self.passportnumber_input = (input(print("What is your passport number?")))
+
         self.cursor = cursor
+
+    def customer_input(self):
+
+        print("Before we proceed to book the flight, we will need to take some details in order to process the transaction\n")
+
+        self.customer_firstname = str(input("Please enter your first name").title())
+        self.customer_surname = str(input("\nPlease enter your surname").title())
+        self.customer_dateofbirth = input("\nPlease enter your date of birth. \n *Please enter the date in this format: YYYY-MM-DD*")
+
+        self.dateofbirth_test()
+
+        sql_customer_query = ("INSERT INTO Customers(FirstName, LastName, DateOfBirth) VALUES(" + str(self.customer_firstname) + str(self.customer_surname) + str(self.customer_dateofbirth) + ")")
+
+        self.cursor.execute(sql_customer_query)
+        self.cursor.commit()
+
+        print("Now that you've successfully entered into the JMS Airport system, let's proceed to the next step " + self.customer_firstname + " !")
+
+    def choice_input(self):
+
+        self.user_choice = int(input("Welcome to the personalised Airport user experience. Please select one of the following options:"
+    "\n 1. Do you want to book a flight for yourself? \n 2. Do you want to book a flight for someone else? \n 3. Change of mind? Do you want to edit a pre-booked flight? \n"))
+        while True:
+            if self.user_choice == 1:
+                self.customer_booking_itself()
+
+            elif self.user_choice == 2:
+                self.customer_booking_someoneelse()
+
+            elif self.user_choice == 3:
+
+
+            else self.user_choice not in [1, 2, 3]:
+                print("Unfortunately, you have selected the wrong output, please try again!")
+            break
+
+    def customer_booking_itself(self): #Ask user what they want, call the test function,
+
+        print("As you have already entered a few details, you only need to add your passport number, and the choice of destination to proceed")
+
+        self.firstname_input = self.customer_firstname
+        self.surname_input = self.customer_surname
+        self.passportnumber_input = (input("What is the person's passport number?"))
+        self.dateofbirth_input = self.customer_dateofbirth
+
+        self.passportnum_test()
+
+        sql_query = ("INSERT INTO BookingDetails(FirstName, LastName, PassportNum, DateOfBirth) VALUES(" + self.firstname_input + self.surname_input + self.passportnumber_input + self.dateofbirth_input + ")")
+
+        self.cursor.execute(sql_query)
+        self.cursor.commit()
+
+        print("You have successfully added a passenger to the flight list")
+
+    def customer_booking_someoneelse(self):
+
+        self.firstname_input = (input("What is the person's first name?").title())
+        self.surname_input = (input("What is the person's surname?").title())
+        self.passportnumber_input = (input("What is the person's passport number?"))
+        self.dateofbirth_input = (input("What is the person's date of birth? *Please enter the date in this format: YYYY-MM-DD*"))
+
+        self.dateofbirth_test()
+        self.passportnum_test()
+
+        sql_query = ("INSERT INTO BookingDetails(FirstName, LastName, PassportNum, DateOfBirth) VALUES(" + self.firstname_input + self.surname_input + self.passportnumber_input + self.dateofbirth_input + ")")
+
+        self.cursor.execute(sql_query)
+        self.cursor.commit()
+
+        print("You have successfully added a passenger to the flight list")
+
 
     def dateofbirth_test(self):
         try:
@@ -29,49 +97,21 @@ class Passengers():
         else:
             print("Sorry, you have not entered the number correctly")
 
-    def input_into_SQL(self):
 
-        sql_query = ("INSERT INTO Customers(FirstName, LastName, DateOfBirth) VALUES(" + str(self.firstname_input) + str(self.surname_input) + str(self.dateofbirth_input) + ")")
-
-        sql_query2 = ("INSERT INTO BookingDetails(PassportNum) VALUES(" + self.passportnumber_input + ")")
-
-        self.cursor.execute(sql_query)
-        self.cursor.execute(sql_query2)
-        self.cursor.commit()
-
-    def passenger_input_in_flights(self):
-
-        sql_query3 = ("INSERT INTO BookingDetails(FirstName, LastName, PassportNum, DateOfBirth) VALUES(" + str(self.firstname_input) + str(self.surname_input) + (self.passportnumber_input) + self.dateofbirth_input + ")") #This is to insert data into the flights information
-
-        self.cursor.execute(sql_query3)
-        self.cursor.commit()
-
-        
-
-
-
-
-
-
-
-
-
-    #
-    # def validating_dateinput(d):
-    #     try:
-    #         if len(d) == 10:
-    #             datetime.strptime(d, '%m/%d/%Y')
-    #             return True
-    #         else:
-    #             return False
-    #
-    #     except ValueError:
-    #         return False
-    #
-    #     print(validate_date('2/26/1000'))
-    #
-
-# databaseconnection()
+#
+#     def passenger_input_in_flights(self):
+#
+#         sql_query3 = ("INSERT INTO BookingDetails(FirstName, LastName, PassportNum, DateOfBirth) VALUES(" + str(self.firstname_input) + str(self.surname_input) + (self.passportnumber_input) + self.dateofbirth_input + ")") #This is to insert data into the flights information
+#
+#         self.cursor.execute(sql_query3)
+#         self.cursor.commit()
+#
+#
+# #Buying a ticket for yourself. Entering details as a customer before buying a ticket
+# #Buying it for someone else - customer details and passenger details
+#
+#
+#
 
 
 
